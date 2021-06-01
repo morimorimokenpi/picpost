a96356f2e43d83b584b98158771122fd44541cc5
Yasunori Kobayashi <52128219+morimorimokenpi@users.noreply.github.com>
2021-06-01 20:18:54 +0900

    [feat] graphql関係の設定

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,19 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    # 全てのデータ取得
+    # 配列なので型を配列型にしてる
+    field :users, [Types::UserType], null: false
+    def users
+      User.all
+    end
+
+    # 引数の番号のデータを取得
+    field :user, Types::UserType, null: false do
+      argument :id, ID, required: true
+    end
+    def user(id:)
+      User.find(id)
     end
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -6,5 +6,6 @@ module Types
     field :password_digest, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :posts, [Types::PostType], null: false
   end
 end
