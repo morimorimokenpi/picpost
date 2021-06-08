9131abee1945e1a3b4ced9d2573a946bfbf6998c
Yasunori Kobayashi <52128219+morimorimokenpi@users.noreply.github.com>
2021-06-08 23:48:33 +0900

    :sparkles: [feat] 一旦投稿はできるように

--- frontend/src/Post.tsx
+++ frontend/src/Post.tsx
@@ -1,50 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 import { useQuery, gql } from "@apollo/client";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import {
-  AppBar,
-  Toolbar,
   Typography,
-  Button,
-  IconButton,
   Card,
   CardHeader,
   CardMedia,
   CardContent,
   Avatar,
   Container,
-  Modal,
-  TextField,
 } from "@material-ui/core";
 import { red } from "@material-ui/core/colors";
-import MenuIcon from "@material-ui/icons/Menu";
+import CreatePostModal from "./CreatePostModal";
 // import { format, parse } from "date-fns";
 
 const rand = () => {
   return Math.round(Math.random() * 20) - 10;
 };
 
-const getModalStyle = () => {
-  const top = 50 + rand();
-  const left = 50 + rand();
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-};
-
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
     cardRoot: {
       maxWidth: 345,
     },
@@ -64,14 +39,6 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     avatar: {
       backgroundColor: red[500],
-    },
-    paper: {
-      position: "absolute",
-      width: 400,
-      backgroundColor: theme.palette.background.paper,
-      border: "2px solid #000",
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3),
     },
   })
 );
@@ -117,52 +84,13 @@ const GET_USERS = gql`
 
 const Post: React.FC = () => {
   const classes = useStyles();
-  const [modalStyle] = useState(getModalStyle);
-  const [open, setOpen] = useState(false);
-
   const { loading, error, data } = useQuery<UserProps>(GET_USERS);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const body = (
-    <div style={modalStyle} className={classes.paper}>
-      <TextField label="title">Title</TextField>
-      <p id="simple-modal-description">
-        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-      </p>
-    </div>
-  );
 
   if (loading) return <div>"ロード中...";</div>;
   if (error) return <div>{error.message}</div>;
   return (
     <>
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              News
-            </Typography>
-            <Button color="inherit" onClick={handleOpen}>
-              新規投稿
-            </Button>
-          </Toolbar>
-        </AppBar>
-      </div>
+      <CreatePostModal />
       {data?.users.map((user) => (
         <Container maxWidth="sm">
           <Card className={classes.cardRoot} key={user.id}>
@@ -196,14 +124,6 @@ const Post: React.FC = () => {
           </Card>
         </Container>
       ))}
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-      >
-        {body}
-      </Modal>
     </>
   );
 };
