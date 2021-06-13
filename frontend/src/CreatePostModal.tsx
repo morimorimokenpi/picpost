13221a0f86a2dea4a0265ea29dbbe797a9e9b259
Yasunori Kobayashi <52128219+morimorimokenpi@users.noreply.github.com>
2021-06-14 01:29:28 +0900

    :recycle: [feat] Headerをコンポーネントに分割

--- frontend/src/CreatePostModal.tsx
+++ frontend/src/CreatePostModal.tsx
@@ -1,16 +1,8 @@
 import React, { useState } from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-  IconButton,
-  Modal,
-  TextField,
-} from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
+import { Button, Modal, TextField } from "@material-ui/core";
 import { useCreatePostMutation } from "./types.d";
+import Header from "./components/Header/Header";
 // import { format, parse } from "date-fns";
 
 const rand = () => {
@@ -105,26 +97,7 @@ const CreatePostModal: React.FC = () => {
 
   return (
     <>
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              News
-            </Typography>
-            <Button color="inherit" onClick={handleOpen}>
-              新規投稿
-            </Button>
-          </Toolbar>
-        </AppBar>
-      </div>
+      <Header onClick={handleOpen} />
       <Modal
         open={open}
         onClose={handleClose}
---
+++ frontend/src/components/Header/styles.ts
@@ -0,0 +1,23 @@
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
+export default makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+    paper: {
+      position: "absolute",
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: "2px solid #000",
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  })
+);
