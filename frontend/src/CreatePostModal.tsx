bec692c1b7afec1a3d30bf73be472fc34afa6aed
Yasunori Kobayashi <52128219+morimorimokenpi@users.noreply.github.com>
2021-06-28 15:45:27 +0900

    :recycle: Refactoring

--- frontend/src/CreatePostModal.tsx
+++ frontend/src/CreatePostModal.tsx
@@ -1,23 +1,16 @@
 import React, { useState } from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import { Button, Modal, TextField } from "@material-ui/core";
+import {
+  Button,
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  Grid,
+} from "@material-ui/core";
 import { useCreatePostMutation } from "./types.d";
 import Header from "./components/Header/Header";
 // import { format, parse } from "date-fns";
-
-const rand = () => {
-  return Math.round(Math.random() * 20) - 10;
-};
-
-const getModalStyle = () => {
-  const top = 50 + rand();
-  const left = 50 + rand();
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-};
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -43,7 +36,6 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const CreatePostModal: React.FC = () => {
   const classes = useStyles();
-  const [modalStyle] = useState(getModalStyle);
   const [open, setOpen] = useState(false);
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
@@ -68,44 +60,56 @@ const CreatePostModal: React.FC = () => {
   };
 
   const handleButtonClick = () => {
+    setOpen(false);
     createPost();
   };
 
   const body = (
-    <div style={modalStyle} className={classes.paper}>
-      <TextField
-        variant="outlined"
-        label="title"
-        onChange={(e) => setTitle(e.target.value)}
-      >
-        Title
-      </TextField>
-      <TextField
-        label="Content"
-        multiline
-        rows={4}
-        variant="outlined"
-        onChange={(e) => setContent(e.target.value)}
-      >
-        Content
-      </TextField>
-      <Button variant="contained" color="primary" onClick={handleButtonClick}>
-        投稿
-      </Button>
-    </div>
+    <>
+      <DialogContent>
+        <Grid>
+          <TextField
+            variant="outlined"
+            label="タイトル"
+            onChange={(e) => setTitle(e.target.value)}
+          >
+            Title
+          </TextField>
+        </Grid>
+        <Grid>
+          <TextField
+            label="コンテント"
+            multiline
+            rows={4}
+            variant="outlined"
+            onChange={(e) => setContent(e.target.value)}
+          >
+            Content
+          </TextField>
+        </Grid>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">
+          戻る
+        </Button>
+        <Button onClick={handleButtonClick} color="primary">
+          作成
+        </Button>
+      </DialogActions>
+    </>
   );
 
   return (
     <>
       <Header onClick={handleOpen} />
-      <Modal
+      <Dialog
         open={open}
         onClose={handleClose}
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
       >
         {body}
-      </Modal>
+      </Dialog>
     </>
   );
 };
